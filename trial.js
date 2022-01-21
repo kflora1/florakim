76af28219966d3d2113a9418eb56f204c78e6085
kflora1 <kflora1@my.yorku.ca>
2022-01-20 20:09:19 -0500

    upd

--- trial.js
+++ trial.js
@@ -4,7 +4,9 @@ function scrollFunction() {
   if (document.body.scrollTop > 95 || document.documentElement.scrollTop > 95) {
     document.getElementById("header").style.fontSize = "90%";
     document.getElementById("header").style.fontWeight = "80%";
+    document.getElementById("header").style.transition = "all linear 0.05s";
     document.getElementById("webTitle").style.fontSize = "1.5rem";
+    document.getElementById("webTitle").style.transition = "all linear 0.05s";
   }
   else{
       document.getElementById("header").style.fontSize = "1rem";
