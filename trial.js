96f6cc02ed8fb2f51b8e32e3349c84fb3cff43c7
kflora1 <kflora1@my.yorku.ca>
2022-01-20 19:50:42 -0500

    upd

--- trial.js
+++ trial.js
@@ -1,1 +1,13 @@
-console.log("hello from trial.js");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 95 || document.documentElement.scrollTop > 95) {
+    document.getElementById("header").style.fontSize = "90%";
+    document.getElementById("header").style.fontWeight = "80%";
+    document.getElementById("webTitle").style.fontSize = "1.5rem";
+  }
+  else{
+      document.getElementById("header").style.fontSize = "1rem";
+      document.getElementById("webTitle").style.fontSize = "48px";
+  }
+}
