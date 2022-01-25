e4c0bc751fc273969682f66b922d44d8ed840abb
kflora1 <kflora1@my.yorku.ca>
2022-01-24 20:55:03 -0500

    upd

--- trial.js
+++ trial.js
@@ -3,9 +3,11 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 95 || document.documentElement.scrollTop > 95) {
     document.getElementById("header").style.fontSize = "90%";
+   
     document.getElementById("header").style.fontWeight = "80%";
     document.getElementById("header").style.transition = "all linear 0.05s";
     document.getElementById("webTitle").style.fontSize = "1.5rem";
+    document.getElementById("webTitle").style.fontStyle = "KumbhSans-Bold"
     document.getElementById("webTitle").style.transition = "all linear 0.05s";
   }
   else{
